44940cf4253f10c389b8dcfadf6e0d411e3de63a
Priyam Das <priyamdas@gmail.com>
2024-01-31 20:38:55 -0800

    adding experiment code

---
+++ exp1B/public/firebaseconfig.js
@@ -0,0 +1,18 @@
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "YOUR INFO HERE",
+
+    authDomain: "YOUR INFO HERE",
+
+    projectId: "YOUR INFO HERE",
+
+    storageBucket: "YOUR INFO HERE",
+
+    messagingSenderId: "YOUR INFO HERE",
+
+    appId: "YOUR INFO HERE"
+
+  };
+
